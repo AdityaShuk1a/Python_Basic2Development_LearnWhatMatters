50a408d7195e192c856887dd50efd09c573e9bc1
AdityaShuk1a <adityasarika0292004@gmail.com>
2025-05-30 16:15:10 +0530

    Variable, DataTypes, TypeCasting and Operators

---
+++ Module 1/Variables_DataType_TypeCasting_Operators.py
@@ -0,0 +1,69 @@
+
+
+#variabls
+
+money = 100
+_name = "Aditya"
+proposalHuaKiNhi = False
+
+# data types
+
+# bool
+# str
+# int
+# float
+
+# print(type(money))
+# print(type(_name))
+# print(type(proposalHuaKiNhi))
+
+# Type Casting
+
+# number = 123
+# print(type(number))
+
+# stringNumber = str(number)
+# print(type(stringNumber))
+
+# stringOne = "123"
+# print(type(stringOne))
+
+# numberOne = int(stringOne)
+# print(type(numberOne))
+
+
+pi = 3.14
+
+stringPI = str(pi)
+print(stringPI)
+
+#operators
+
+numOne = 1
+numTwo = 2
+
+# print(numOne + numTwo) # 3
+# print(numOne - numTwo) # -1
+# print(numOne * numTwo) # 2
+# print(numOne / numTwo) # 0.5
+# print(numOne // numTwo) # 0
+# print(numOne % numTwo) # 1
+
+# print(10 % 3) # 1
+# print(10 / 3) # 3.33
+# print(10 // 3) # 3
+
+# Assignment Operators
+
+numThree = 1
+numThree+=numOne # 2
+# numThree = numThree + numOne
+# print(numThree)
+
+# numThree-=numOne # 1
+numThree *= numTwo # 4
+numThree = 20
+numThree /= 10 # 2
+print(numThree) 
+
+print(2/2) # 1.0
